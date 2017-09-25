0801fe92cf818f0902c2ac4773000c8690262c1c
Guest User <Guest@Epicodus-6E.local>
2017-09-25 16:20:46 -0700

    add function to call a random movie from API

--- js/scripts-interface.js
+++ js/scripts-interface.js
@@ -3,15 +3,10 @@ import { scripts } from "./../js/scripts.js";
 
 $(document).ready(function() {
 
-  function randMovie() {
-    min = Math.ceil(50);
-    max = Math.floor(450000);
-    console.log(min);
-    console.log(max);
-    result = Math.floor(Math.random() * (max - min)) + min;
-  }
-  function displayMovieData(movieId) {
-    $('#displayQuery').append(`<p>Your movie id is: ${movieId}`);
+  function displayMovieData(movieId, movieName) {
+    $('#displayMovieId').append(`<p>Your movie id is: ${movieId}`);
+    $('#displayMovieName').append(`<p>Your movie id is: ${movieName}`);
+
   }
 
 
--- js/scripts.js
+++ js/scripts.js
@@ -2,18 +2,26 @@ let apiKey = require('./../.env').apiKey;
 
 export let scripts = {
 
+
+
   findMovie: function(displayMovieData) {
-    let apiRequest1 = fetch(`https://api.themoviedb.org/3/movie/477651?api_key=${apiKey}&language=en-US`)
+
+
+    let min = Math.ceil(65);
+    let max = Math.floor(45000);
+    let result =  Math.floor((Math.random() * (max - min)) + min);
+    let apiRequest1 = fetch(`https://api.themoviedb.org/3/movie/${result}?api_key=${apiKey}&language=en-US`)
     .then((res) => {
       res.json().then((test) => {
         console.log(test);
         let movieId = test.id;
+        let movieName = test.original_title;
+
         console.log("ID of latest movie in DB: " + movieId);
-        displayMovieData(movieId);
+        displayMovieData(movieId, movieName);
       });
     })
     .catch(console.log);
   }
-
 
 };
